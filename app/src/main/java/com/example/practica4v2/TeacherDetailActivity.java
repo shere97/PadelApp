c1bd22bb2e67093ee3da3aa8a9409ca547ebf0e8
Jan Cano <57530243+shere97@users.noreply.github.com>
2024-06-22 18:58:33 +0200

    Metodo para hacer reservas realizado

--- app/src/main/java/com/example/practica4v2/TeacherDetailActivity.java
+++ app/src/main/java/com/example/practica4v2/TeacherDetailActivity.java
@@ -9,19 +9,33 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.example.practica4v2.model.Reserva;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class TeacherDetailActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -63,16 +77,12 @@ public class TeacherDetailActivity extends AppCompatActivity {
 
 
         btnReserve.setOnClickListener(v -> {
-            mostrarDialogoConfirmacion();
+            mostrarDialogoConfirmacion(name);
         });
 
     }
 
-    private void mostrarDialogoConfirmacion() {
-
-        Calendar calendario = Calendar.getInstance();
-        int horaActual = calendario.get(Calendar.HOUR_OF_DAY);
-        int minutoActual = calendario.get(Calendar.MINUTE);
+    private void mostrarDialogoConfirmacion(String teacherName) {
 
         TimePicker timePicker = new TimePicker(this);
         timePicker.setIs24HourView(true);
@@ -92,7 +102,7 @@ public class TeacherDetailActivity extends AppCompatActivity {
                 int minute = timePicker.getCurrentMinute();
 
                 // Aquí se ejecuta cuando se selecciona la hora y se hace clic en OK
-                confirmarReserva(); // Llamar a método para confirmar reserva
+                confirmarReserva(teacherName, hourOfDay, minute); // Llamar a método para confirmar reserva
             }
         });
 
@@ -109,9 +119,31 @@ public class TeacherDetailActivity extends AppCompatActivity {
 
     }
 
-    private void confirmarReserva() {
+    private void confirmarReserva(String teacher_name, int hourOfDay, int minute) {
 
+        String horaSeleccionada = String.format("%02d:%02d", hourOfDay, minute);
+        String userId = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid(); // Obtenemos el ID del usuario actual
+        System.out.println("El USUARIO ES:" + firebaseAuth.getCurrentUser().getUid());
+        DatabaseReference reservasRef = databaseRef.child("reservas").child(userId).push();
+
+        String reservaId = reservasRef.getKey();
+
+        Reserva reserva = new Reserva(reservaId, teacher_name, firebaseAuth.getCurrentUser().getEmail(), horaSeleccionada);
+
+        // Guardamos la reserva en la base de datos
+        reservasRef.setValue(reserva)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(TeacherDetailActivity.this, "Reserva confirmada para " + horaSeleccionada, Toast.LENGTH_SHORT).show();
+                        // Aquí puedes navegar a otra actividad o hacer cualquier acción adicional
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(TeacherDetailActivity.this, "Error al confirmar la reserva", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
-
-        //TODO : Hacer la conexion con la base de datos
 }
--- app/src/main/java/com/example/practica4v2/model/Reserva.java
+++ app/src/main/java/com/example/practica4v2/model/Reserva.java
@@ -1,4 +1,34 @@
 package com.example.practica4v2.model;
 
 public class Reserva {
+
+    private String idReserva;
+    private String teacher_name;
+
+    private String user_name;
+
+    private String horaSeleccionada;
+
+    public String getIdReserva() {
+        return idReserva;
+    }
+
+    public String getTeacher_name() {
+        return teacher_name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public String getHoraSeleccionada() {
+        return horaSeleccionada;
+    }
+
+    public Reserva(String idReserva, String teacher_name, String user_name, String horaSeleccionada) {
+        this.idReserva = idReserva;
+        this.teacher_name = teacher_name;
+        this.user_name = user_name;
+        this.horaSeleccionada = horaSeleccionada;
+    }
 }
